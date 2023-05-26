78c4a6e56c779c7bcc460096ec0e18e059e47d7c
José Luis Huacles <joseluishuaclesunocc@gmail.com>
2023-05-26 13:28:12 -0500

    Agregados archivos base, modificado main.yml

--- buscara.py
+++ buscara.py
@@ -1,39 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
-# URL de la página web
-url = "https://example.com"  # Reemplaza con la URL correcta
+def distritalxProvincia(provincias):
+    for provincia in provincias:
+        df = pd.read_excel(f"central_table-{provincia}.xlsx")
+        df1 = pd.read_excel("indicadores_dengue_diario_distrito.xlsx")
+        df1.replace({"LA TINGUIÑA": "LA TINGUINA"})
+        df1.replace("PARIÑÃ‰AS", "PARIÑAS",inplace=True)
+        df1.replace('PARIÑÉAS', 'PARIÑAS', inplace=True)
 
-# Realizar la solicitud GET a la página web
-response = requests.get(url)
+        dt = datetime.now()
+        str_time = dt.strftime("%d-%m-%Y")
+        ##Filtrando por departamentos
+        df1 = df1[df1['Departamento'] == provincia.upper()]
+        dfFinal = pd.merge(df, df1, on="Distrito", how="outer")
+        dfFinal = dfFinal.rename(columns={'Incidencia* x100mil': 'Incidencia*'})
+        dfFinal = dfFinal.rename(columns={'Casos*': 'Personas contagiadas'})
+        dfFinal = dfFinal.rename(columns={'Fallecidos*': 'Personas fallecidas'})
+        dfFinal.replace(0.0, '', inplace=True)
+        
 
-# Crear el objeto BeautifulSoup a partir del contenido HTML
-soup = BeautifulSoup(response.content, "html.parser")
+        dfFinal.to_csv(f"dengue_diario-{provincia}.csv", columns=['id','Distrito','Personas contagiadas','Incidencia*','Personas fallecidas'], index=False, encoding='utf-8')
 
-# Encontrar todos los elementos <a> con la función onclick
-elements = soup.find_all("a", onclick=True)
+provincias = ["Lima", "Piura", "Loreto", "Ica", "Ucayali"]
 
-# Variable para almacenar si se encontró el texto en algún elemento
-found_text = False
-
-# Recorrer cada elemento <a> y verificar si el texto está presente en la función onclick
-for element in elements:
-    onclick_value = element["onclick"]
-    if "indicadores_dengue_diario_distrito.xlsx" in onclick_value:
-        found_text = True
-
-        # Hacer clic en el elemento utilizando Selenium
-        driver = webdriver.Chrome()  # Reemplaza con el controlador de navegador adecuado
-        driver.get(url)
-        driver.execute_script(onclick_value)
-        driver.quit()
-
-        break
-
-# Continuar con el código después de hacer clic en el elemento <a>, si es necesario
-if found_text:
-    # Código adicional después de hacer clic en el elemento <a>
-    print("Se hizo clic en el elemento <a>.")
-else:
-    print("El texto no está presente en ningún elemento <a>.")
+distritalxProvincia(provincias)
